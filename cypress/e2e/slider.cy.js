22d6c527ec8d906761ca723504bafc56c552ff53
bszarlowicz <b.szarlowicz@gmail.com>
2025-03-26 12:23:55 +0100

    fix

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -1,6 +1,6 @@
 describe('Swiper Gallery Test - basic #1', function () {
   it('Checks if second slide contains "United Kingdom"', function () {
-    cy.visit('http://localhost:3001');
+    cy.visit('http://localhost:3000');
     cy.get('.swiper-button-next').click();
     cy.get('.swiper-slide-active').should('contain', 'United Kingdom');
   });
@@ -8,7 +8,7 @@ describe('Swiper Gallery Test - basic #1', function () {
 
 describe('Swiper Gallery Test - basic #2', function () {
   it('Checks if third slide contains "Paris"', function () {
-    cy.visit('http://localhost:3001');
+    cy.visit('http://localhost:3000');
     cy.get('.swiper-button-next').click();
     cy.wait(2000);
     cy.get('.swiper-button-next').click({ force: true });
@@ -19,7 +19,7 @@ describe('Swiper Gallery Test - basic #2', function () {
 
 describe('Swiper Gallery Test #1', function () {
   it('Check whether user can swipe trough slides "', function () {
-    cy.visit('http://localhost:3001');
+    cy.visit('http://localhost:3000');
     cy.get('.swiper-button-next').click();
     cy.wait(2000);
     cy.get('.swiper-slide-active').should('exist');
@@ -31,7 +31,7 @@ describe('Swiper Gallery Test #1', function () {
 
 describe('Swiper Gallery Test #2', function () {
   it('Check whether the description of each slide is displayed correctly."', function () {
-    cy.visit('http://localhost:3001');
+    cy.visit('http://localhost:3000');
     cy.get('.swiper-slide-active .card-description').should('exist');
     cy.get('.swiper-slide-active .card-description h1').should('exist').and('have.text', 'Rome')
     cy.get('.swiper-slide-active .card-description p').should('exist').and('have.text', 'Italy')
